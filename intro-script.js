3518ab433669a2fd9be9bf4a46bb2ea573ba140d
KParhar <coderk2001@gmail.com>
2018-04-30 12:28:24 -0400

    Initial commit

---
+++ intro-script.js
@@ -0,0 +1,38 @@
+//Constants
+var blinkTimeMs = 500;
+var typeSpeed = 100; //Default is 100
+var introFadeSpeed = 400;
+var contentFadeSpeed = 2500;
+
+$(document).ready(function(){
+	
+	setInterval ('blink()', blinkTimeMs);
+	
+	typeEffect("#introTypeEffect", "KParhar.exe<br>Starting program...<br>", typeSpeed, function() {
+		$("#intro").delay(100).fadeOut(introFadeSpeed);
+		$("#content").fadeTo(contentFadeSpeed, 1);
+		
+		var vpH = $(window).height() - $("#navbar").height();
+		$("#about").css("height", vpH + "px");
+		$("#testimonials").css("height", vpH + "px");
+		$("#projects").css("height", vpH + "px");
+		$("#contact").css("height", vpH + "px");
+	});
+});
+
+function blink() {
+	$("#intro-selector").toggle();
+}
+
+var captionLength = 0;
+function typeEffect(id, value, speed, callback) {
+	$(id).html(value.substr(0, captionLength++));
+	
+	if(captionLength < value.length+1) {
+        setTimeout('typeEffect("' + id + '", "' + value + '", ' + speed + ', ' + callback + ')', speed);
+    } else {
+        captionLength = 0;
+        value = '';
+		callback();
+    }
+}
---
+++ scrollEffect-script.js
@@ -0,0 +1,21 @@
+//Constants
+var scrollSpeed = 750;
+
+$(document).ready(function(){
+	$("#Logo").click(function(){ scrollTo("#landingScreen", scrollSpeed); });
+	$("#home-link").click(function(){ scrollTo("#landingScreen", scrollSpeed); });
+	$("#about-link").click(function(){ scrollTo("#about", scrollSpeed); });
+	$("#testimonials-link").click(function(){ scrollTo("#testimonials", scrollSpeed); });
+	$("#projects-link").click(function(){ scrollTo("#projects", scrollSpeed); });
+	$("#contact-link").click(function(){ scrollTo("#contact", scrollSpeed); });
+	
+	$("#subtextButton").click(function(){ scrollTo("#about", scrollSpeed); });
+});
+
+//TODO
+//Make the scroll ease in
+function scrollTo(divID, speed) {
+	$('html, body').animate({
+        scrollTop: $(divID).offset().top - $("#navbar").height()
+    }, speed, "easeInOutCubic");
+}
