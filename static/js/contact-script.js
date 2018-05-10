89a1908d5bb3693b65da826ced8ebabec63342cc
KParhar <coderk2001@gmail.com>
2018-05-10 12:54:40 -0400

    2018-05-10 12:54PM

--- static/js/contact-script.js
+++ static/js/contact-script.js
@@ -1,14 +1,7 @@
 $(document).ready(function(){
-	$("#contact-submit").on('click', function(event) {
-		
-		$("#contact-submit").addClass("shrink");
-		$("#contact-submit").one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
-			function(event) {
-				
-			}
-		);
-		
+	$("#contact-submit").on("click", function(event) {
 		if($("#contact-name").val() != "" && $("#contact-email").val() != "" && $("#contact-subject").val() != "" && $("#contact-msg").val() != "") {
+			startSendAnimation();
 			$("#msg-status").text("Sending Message...").show();
 			$.ajax({
 				data : {
@@ -21,12 +14,61 @@ $(document).ready(function(){
 				url : "/email"
 			})
 			.done(function(data) {
+				endSendAnimation();
 				$("#msg-status").text(data.Status).show();
 			});
 		} else {
+			checkBlankForm();
+			
+			startShakeAnimation();
+			setTimeout(endShakeAnimation, 500);
 			$("#msg-status").text("Please fill in all inputs.").show();
 		}
 		event.preventDefault();
 	});
+	
+	$(".contact-input").focus(function(event){
+		$(this).removeClass("errorBorder");
+		setTimeout(checkBlankForm(this.id), 50);
+	});
+	
+	$(".contact-input").keyup(function(event){
+		$(this).removeClass("errorBorder");
+		setTimeout(checkBlankForm(this.id), 10);
+	});
 });
 
+function checkBlankForm(i) {
+	id = "#" + i;
+	if($(id).val() == "") { 
+		$(id).addClass("errorBorder");
+	}
+}
+
+function startSendAnimation() {
+	resetAnimation();
+	$("#contact-submit").removeClass("returnSendAnimation");
+	$("#contact-submit").addClass("sendAnimation");
+}
+
+function endSendAnimation() {
+	$("#contact-submit").removeClass("sendAnimation");
+	$("#contact-submit").addClass("returnSendAnimation");
+}
+
+function resetAnimation() {
+	var e = document.getElementById("contact-submit");
+	e.style.animation = 'none';
+	e.offsetHeight;
+	e.style.animation = null; 
+}
+
+function startShakeAnimation() {
+	$("#contact-submit").removeClass("sendAnimation");
+	$("#contact-submit").removeClass("returnSendAnimation");
+	$("#contact-submit").addClass("shake");
+}
+
+function endShakeAnimation() {
+	$("#contact-submit").removeClass("shake");
+}
